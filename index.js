a319bb9fbaec590f03002a9246a29ed3fa2222f4
usmcqueen <pixiedustyoucanfly@gmail.com>
2023-06-05 14:42:02 -0400

    First commit blog-api-side for capstone

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "http://127.0.0.1:3000",
     credentials: true,
     exposedHeaders: ["access_token"],
   })
